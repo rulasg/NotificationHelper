ddc843ebaec3c1fe2bb68a14ed9b4aa36ff373ce
Raúl (Dibildos) González <rulasg@github.com>
2026-03-24 07:14:15 +0100

    feat: enhance Show-Notifications function with additional parameters for improved filtering

--- public/shownotifications.ps1
+++ public/shownotifications.ps1
@@ -2,14 +2,34 @@ function Show-Notifications {
     [CmdletBinding()]
     [alias('sn')]
     param(
-        [Parameter(Position=0)][string]$Id,
+        [Parameter(Position = 0)][string]$Id,
         [Parameter()][string]$Url,
         [Parameter()][string]$Title,
-        [Parameter()][switch]$Force
+        [Parameter()][ValidateSet("Issue","Discussion","PullRequest","Release")][string]$Type,
+        [Parameter()][ValidateSet(
+            "assign",
+            "subscribed",
+            "comment",
+            "author",
+            "team_mention",
+            "mention",
+            "state_change",
+            "manual"
+        )][string]$Reason,
+
+        [Parameter()][switch]$IncludeUnRead,
+        [Parameter()][string]$RepoName,
+        [Parameter()][string]$RepoOwner,
+        [Parameter()][switch]$Force,
+        [Parameter()][string[]]$Sort
     )
 
-    $list = Get-Notification -Id $Id -Url $Url -Title $Title -Force:$Force
+    # Default is show ownly the unread
 
-    $list.Values | Select-Object Id,type,Reason,Title,RepoName,RepoOwner,Updated
+    $list = @(getNotification -Id $Id | Select-Notification -Url $Url -Title $Title -Type $Type -Reason $Reason -IncludeUnRead:$IncludeUnRead -Force:$Force -RepoName $RepoName -RepoOwner $RepoOwner)
+
+    "Listing [$($list.Length)] notifications." | Write-MyDebug -Section "Show-Notifications"
+
+    $list | Select-Object Id, type, Reason, UnRead, Title, RepoName, RepoOwner, Updated | Sort-Object $Sort | Format-Table -AutoSize
 
 } Export-ModuleMember -Function Show-Notifications -Alias sn
