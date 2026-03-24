b11e45a5bf401666882c77d57dbafc2c4d010453
Raúl (Dibildos) González <rulasg@github.com>
2026-03-24 07:14:44 +0100

    refactor(Resolve-Notification): improve notification resolution logic and error handling

--- public/resolvenotification.ps1
+++ public/resolvenotification.ps1
@@ -1,23 +1,62 @@
+
+Set-MyInvokeCommandAlias -Alias ResolveNotification -Command "Invoke-ResolveNotification -Action {action} -Url {url}"
+
 function Resolve-Notification {
     [CmdletBinding()]
     [alias('rn')]
     param (
-        [parameter(ValueFromPipeline, Position=0)][object]$Notification,
-        [parameter()][switch]$MarkAsDone
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)][string]$Id,
+        [Parameter(Position = 1)][ValidateSet("Done","Read")][string]$Action = "Read"
     )
-
-    begin {
-        $methord = if($MarkAsDone) { "DELETE" } else { "PATCH" }
-    }
 
     process {
 
+        "Resolving notification with id [$Id] action [$Action] ..." | Write-MyDebug -Section "Resolve-Notification"
+
+        $Notification = getNotification -Id $Id
+        
+        if(-not $Notification){
+            "No notification found with id [$Id]" | Write-MyDebug -Section "Resolve-Notification"
+            return $false
+        }
+        
+        if($action -eq "Read" -and $Notification.IsRead){
+            "Notification with id [$Id] is already marked as read." | Write-MyDebug -Section "Resolve-Notification"
+            return $true
+        }
+        
         $url = $Notification.threadUrl
 
-        Write-Verbose -Message "Resolving notification with id [$($Notification.id)] using method [$methord] on url [$url] ..."
+        $response = Invoke-MyCommand -Command ResolveNotification -Parameters @{ action = $Action; url = $url }
         
-        $response = gh api --method $methord $url
+        if ($response) {
+            resolveNotification -Id $Id -Action $Action
+            return $true
+        }
         
-        return $response
+        "Something went wrong while resolving notification with id [$Id]. $response" | Write-MyDebug -Section "Resolve-Notification"
+        return $false
     }
 } Export-ModuleMember -Function Resolve-Notification -Alias rn
+
+function Invoke-ResolveNotification {
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Position = 0)][string]$Url,
+        [Parameter(Mandatory)][ValidateSet("Done","Read")][string]$Action
+    )
+
+    $methord = if ($Action -eq "Done") { "DELETE" } else { "PATCH" }
+
+    "Resolving [$methord] for [$Url] >>>" | Write-MyDebug -Section "Invoke-ResolveNotification"
+    $response = gh api --method $methord $url
+    "Resolving [$methord] for [$Url] <<<" | Write-MyDebug -Section "Invoke-ResolveNotification"
+
+    if($null -eq $response){
+        "Resolve successful [$methord] for [$Url]" | Write-MyDebug -Section "Invoke-ResolveNotification"
+        return $true
+    } else {
+        "Failed to resolve notification [$methord] for [$Url]. Response: $response" | Write-MyDebug -Section "Invoke-ResolveNotification"
+        return $false
+    }
+} Export-ModuleMember -Function Invoke-ResolveNotification
