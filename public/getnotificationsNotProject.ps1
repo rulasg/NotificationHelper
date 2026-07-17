71c82827e9655b556a6da51e77ca1fef0e60fad9
Raúl (Dibildos) González <rulasg@github.com>
2026-07-17 12:06:58 +0200

    feat(Show-Notifications): add filtering by title in Show-NotificationsNotInProject

--- public/getnotificationsNotProject.ps1
+++ public/getnotificationsNotProject.ps1
@@ -46,7 +46,8 @@ function Show-NotificationsNotInProject{
         [Parameter()][string]$RepoName,
         [Parameter()][string]$RepoOwner,
         [Parameter()][switch]$Force,
-        [Parameter()][switch]$PassThru
+        [Parameter()][switch]$PassThru,
+        [Parameter(Position=0)][string]$Filter
     )
 
     $params = @{
@@ -57,8 +58,15 @@ function Show-NotificationsNotInProject{
         Force = $Force
     }
 
+    # Get the notifications not in the project
     $n = Get-NotificationsNotInProject @params
 
+    # Filter by title if a filter is provided
+    if (-Not [string]::IsNullOrEmpty($Filter)) {
+        $n = $n | Where-Object { $_.Title -like "*$Filter*" }
+    }
+
+    # If PassThru is specified, return the raw notifications; otherwise, select specific properties for display
     if ($PassThru) {
         return $n
     }
